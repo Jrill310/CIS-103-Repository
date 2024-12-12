3ee0e4ee60d5c638f5143c3c66596c23ad38b228
Jrill310 <jrilling1@student.ccc.edu>
2024-12-12 10:08:52 -0600

    Add files via upload

---
+++ P12.py
@@ -0,0 +1,17 @@
+x=0
+a=0
+monthlist = ['January','February','March','April','May','June','July','Augest','September','October','November','December']
+rainlist = []
+while x < 12:
+    print ('Input',monthlist[x]+"'s rainfall data",)
+    Rainfall = float (input ('->'))
+    rainlist.append (Rainfall)
+    x=x+1
+print (rainlist)
+average = sum(rainlist) / 12
+print ('highest: ',max(rainlist))
+print ('lowest: ',min(rainlist))
+print ('Total: ',sum(rainlist))
+print ('Average: ',average)
+
+    
